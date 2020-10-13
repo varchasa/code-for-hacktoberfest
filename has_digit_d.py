3026661c6c4a03d2d6e3d3a23b1662fbdfe35326
KoushikSahu <koushiksahu68@gmail.com>
2020-10-13 19:49:14 +0530

    Added solution to issue #9

---
+++ has_digit_d.py
@@ -0,0 +1,23 @@
+def has_digit_d(num, d):
+    num = str(num)
+
+    for digit in num:
+        if int(digit) == d:
+            return True
+    
+    return False
+
+if __name__ == '__main__':
+    print("Enter value of n")
+    n = int(input())
+
+    print("Enter value of d")
+    d = int(input())
+
+    ans = 0
+
+    for i in range(0, n+1):
+        if(has_digit_d(i, d)==True):
+            ans += 1
+
+    print("Number of integer between 0 and", n, "having digit", d, "is:", ans)
